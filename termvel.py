7eea4c644c226174cde0326133c06585ac42b46a
Ramanakumar Sankar <ramanakumar.shankar@gmail.com>
2016-08-25 15:20:40 -0400

    Added gamma calculation for triple point ref pressure

--- termvel.py
+++ termvel.py
@@ -2,18 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 from scipy.optimize import curve_fit
-import pp
-import multiprocessing
-
-## Initialize parallel python
-ppservers = ()
-ppservers = ()
-
-ncpu = int(np.min([6,multiprocessing.cpu_count()]))
-
-job_server = pp.Server(ncpu, ppservers = ppservers)
-
-print("Parallel server initialized! Running with ", job_server.get_ncpus()," CPUs")
 
 ## Functions
 def molarmass(frac, mass):
@@ -34,6 +22,15 @@ def least_squares(x, y, k = 1):
 	a = np.dot(np.linalg.inv(np.dot(X,X.T)),np.dot(X,y.T))
 
 	return a
+	
+def newtons(func, val, x0, thresh = 0.1, h = 0.0001):
+	## Root finding algorithm based on first order derivatives (Newton-Rhapsons' Method)
+	diff = 100.
+	while diff > thresh:
+		grad = (func(x0 + h) - func(x0))/h
+		x0 = x0 - (func(x0)-val)/grad
+		diff = func(x0) - val
+	return x0
 	
 def dynvisc(planet):
 	## C Palotai's code from EPIC model (epic_microphysics_funcs.c)
@@ -114,7 +111,10 @@ def vT_calc(planet, species, phase, p, D, planet_data, spec_data):
 
 		return ((1./D)*(dynvisc*Re)/(rho_air))
 
-def vT_fit(x, a, b, c):
+def vT_fit (x, a, b):
+	return (a + b*x)
+		
+def vT_fit_old(x, a, b, c):
 	return( a + b*x[:,0] + c*x[:,1])
 	
 global planet_data, spec_data
@@ -125,8 +125,8 @@ planet_data["Saturn"] = {"g":10.5, "xH2": 0.96,"xHe": 0.04, "datfile": "saturn_d
 
 ## Species arrays
 spec_data = {}
-spec_data["H2O"] = {"mass":18., "ant_liq": [11.079, -2261.1], "ant_ice": [12.61,-2681.18], "q": 0.001420, "rho_ice": 917.0, "rho_liq": 1000., "A_Ae_snow": 1.05, "A_Ae_ice": 1.}
-spec_data["NH3"] = {"mass":17., "ant_liq": [10.201, -1248.0], "ant_ice": [11.90,-1588.00], "q": 0.000187, "rho_ice": 786.8, "rho_liq":  733., "A_Ae_snow": 1.05, "A_Ae_ice": 1.}
+spec_data["H2O"] = {"mass":18., "ant_liq": [11.079, -2261.1], "ant_ice": [12.61,-2681.18], "q": 0.001420, "rho_ice": 917.0, "rho_liq": 1000., "A_Ae_snow": 1.05, "A_Ae_ice": 1., "tp": 273.16}
+spec_data["NH3"] = {"mass":17., "ant_liq": [10.201, -1248.0], "ant_ice": [11.90,-1588.00], "q": 0.000187, "rho_ice": 786.8, "rho_liq":  733., "A_Ae_snow": 1.05, "A_Ae_ice": 1., "tp": 195.20}
 
 ## T-P profiles
 ## tp_profile["planet"]["pressure mbar"][variable sought] 
@@ -171,6 +171,14 @@ for planet in planet_data.keys():
 	## Save minimum and maximum values of P used in interpolation
 	planet_data[planet]["Prange"] = np.asarray([data[0,0],data[-1,0]])
 	
+	planet_data[planet]["tpPressure"] = {}
+	
+	for species in spec_data.keys():
+		print("\tFinding fit for reference triple point pressure for %s"%(species))
+		tp = spec_data[species]["tp"]
+		tpP = 10.**(newtons(fT, tp, np.log10(5000.)))
+		print("\tTriple point found at %.2f mbar"%(tpP))
+		planet_data[planet]["tpPressure"][species] = tpP
 	'''
 	#Plot the data
 	plt.figure()
@@ -200,14 +208,27 @@ vT = {}
 x = {}
 y = {}
 gamma = {}
+chi = {}
+
+x2 = {}
+y2 = {}
+gamma2 = {}
+chi2 = {}
 
 ## Run terminal velocity for each species for each planet
 for species in spec_data.keys():
 	print("\nSpecies: %s"%(species))
 	vT[species] = {}
+	
 	x[species] = {}
 	y[species] = {}
 	gamma[species] = {}
+	chi[species] = {}
+	
+	x2[species] = {}
+	y2[species] = {}
+	gamma2[species] = {}
+	chi2[species] = {}
 	
 	for planet in planet_data.keys():
 		print("\tPlanet: %s"%(planet))
@@ -215,6 +236,10 @@ for species in spec_data.keys():
 		x[species][planet] = {}
 		y[species][planet] = {}
 		gamma[species][planet] = {}
+		
+		x2[species][planet] = {}
+		y2[species][planet] = {}
+		gamma2[species][planet] = {}
 		
 		for phase in ["rain","ice","snow"]:
 			print("\t\tPhase: %s"%(phase))
@@ -233,19 +258,34 @@ for species in spec_data.keys():
 			
 			print("\t\t\tCalculating terminal velocity profile")
 			## Parallelize the calculation script
-			
 			for p in P[species]:
 				vT[species][planet][phase][p] = np.zeros(len(D))
-				
-			jobs = [(i, job_server.submit(vT_calc,(planet,species, phase, p, D, planet_data, spec_data),(mbartopa,),("import numpy as np",))) for i, p in enumerate(P[species])]
 			
-			for i, job in jobs:
-				vT_d = job()
+			for i,p in enumerate(P[species]):
+				vT_d = vT_calc(planet,species, phase, p, D, planet_data, spec_data)
 				vT[species][planet][phase][P[species][i]] = vT_d
-		
+			vT_d = vT_calc(planet,species, phase, planet_data[planet]["tpPressure"][species], D, planet_data, spec_data)
+			vT[species][planet][phase][planet_data[planet]["tpPressure"][species]] = vT_d
+			gamma_vals = []
+			for p in P[species]:
+				if(p != Pref):
+					gamma_p = np.average(np.log(vT[species][planet][phase][p]/vT[species][planet][phase][Pref])/np.log(Pref/p))
+					gamma_vals.append(gamma_p)
+			
+			gamma[species][planet][phase] = np.average(gamma_vals)
+			
+			gamma_vals = []
+			Pref2 = planet_data[planet]["tpPressure"][species]
+			for p in P[species]:
+				if(p != Pref2):
+					gamma_p = np.average(np.log(vT[species][planet][phase][p]/vT[species][planet][phase][Pref2])/np.log(Pref2/p))
+					gamma_vals.append(gamma_p)
+			
+			gamma2[species][planet][phase] = np.average(gamma_vals)
 			## Convert 2D array of pressure and particle size to 1D
 			## Also convert vT to 1D array
 			## For fitting
+			
 			
 			xData = []
 			zData = []
@@ -254,8 +294,8 @@ for species in spec_data.keys():
 			for i in range(len(P[species])):
 				for j in range(len(D)):
 					#index = (i*len(P[species])) + j
-					xData.append(((Pref/P[species][i]),D[j]))
-					zData.append(vT[species][planet][phase][P[species][i]][j])
+					xData.append(D[j])
+					zData.append(vT[species][planet][phase][P[species][i]][j]/((Pref/P[species][i])**gamma[species][planet][phase]))
 			xData = np.asarray(xData)
 			zData = np.asarray(zData)
 
@@ -264,13 +304,35 @@ for species in spec_data.keys():
 			
 			## Save the parameters
 			x[species][planet][phase] = 10.**par[0]
-			y[species][planet][phase] = par[2]
-			gamma[species][planet][phase] = par[1]
+			y[species][planet][phase] = par[1]
+			
+			## Run with new triple-point reference pressure
+			xData2 = []
+			zData2 = []
+			
+			Pref2 = planet_data[planet]["tpPressure"][species]
+			print("\t\tCreating array to fit")
+			for i in range(len(P[species])):
+				for j in range(len(D)):
+					#index = (i*len(P[species])) + j
+					xData2.append(D[j])
+					zData2.append(vT[species][planet][phase][P[species][i]][j]/((Pref2/P[species][i])**gamma2[species][planet][phase]))
+			xData2 = np.asarray(xData2)
+			zData2 = np.asarray(zData2)
+			
+			print("\t\tFitting with triple point reference")
+			par2, cov2 = curve_fit(vT_fit, np.log10(xData2),np.log10(zData2))
+			
+			
+			x2[species][planet][phase] = 10.**par2[0]*((Pref2/Pref)**(gamma2[species][planet][phase]))
+			y2[species][planet][phase] = par2[1]
+			
+			
 	
 ## Plot parameters	
 pltspec = "H2O"
-pltplanet = "Saturn"
-pltphase = "rain"
+pltplanet = "Jupiter"
+pltphase = "snow"
 pltP = 1000.
 
 plt.figure()
@@ -278,15 +340,15 @@ plt.plot(vT[pltspec][pltplanet][pltphase]["D"]*1000.,vT[pltspec][pltplanet][pltp
 fitted = x[pltspec][pltplanet][pltphase]*(vT[pltspec][pltplanet][pltphase]["D"]**y[pltspec][pltplanet][pltphase])*(Pref/pltP)**gamma[pltspec][pltplanet][pltphase]
 plt.plot(vT[pltspec][pltplanet][pltphase]["D"]*1000.,fitted,'k-')
 
-printplanet = "Saturn"
+printplanet = "Jupiter"
 for species in spec_data.keys():
 	print(species)
 	print("----------------")
 	for phase in ["ice","snow","rain"]:
 		print("Phase: %s"%(phase))
-		print("x: %.4f"%(x[species][printplanet][phase]))
-		print("y: %.4f"%(y[species][printplanet][phase]))
-		print("gamma: %.4f"%(gamma[species][printplanet][phase]))
+		print("x: %.4f \t x2 : %.4f"%(x[species][printplanet][phase],x2[species][printplanet][phase]))
+		print("y: %.4f \t y2 : %.4f"%(y[species][printplanet][phase],y2[species][printplanet][phase]))
+		print("gamma: %.4f \t gamma2 : %.4f"%(gamma[species][printplanet][phase],gamma2[species][printplanet][phase]))
 		print()
 	print()
 plt.show()
