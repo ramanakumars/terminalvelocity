65f29f3b0c048f5adbd7256b84a51d848ef5eb6d
Ramanakumar Sankar <ramanakumar.shankar@gmail.com>
2016-09-17 18:47:27 -0400

    Formatting fixed + new comments

--- termvel.py
+++ termvel.py
@@ -1,4 +1,3 @@
-## THIS IS FROM 16 SEP MEETING ##
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
@@ -125,6 +124,7 @@ planet_data["Jupiter"]  = {"g": 22.31, "xH2": 0.864, "xHe": 0.136,"datfile": "ju
 planet_data["Saturn"] = {"g":10.5, "xH2": 0.96,"xHe": 0.04, "datfile": "saturn_data.csv", "Pref": 1000.} 		
 
 ## Species arrays
+## ant_ice and ant_liq are unnecessary and exist in case we need them for another calculation
 spec_data = {}
 spec_data["H2O"] = {"mass":18., "ant_liq": [11.079, -2261.1], "ant_ice": [12.61,-2681.18], "q": 0.001420, "rho_ice": 917.0, "rho_liq": 1000., "A_Ae_snow": 1.05, "A_Ae_ice": 1., "tp": 273.16}
 spec_data["NH3"] = {"mass":17., "ant_liq": [10.201, -1248.0], "ant_ice": [11.90,-1588.00], "q": 0.000187, "rho_ice": 786.8, "rho_liq":  733., "A_Ae_snow": 1.05, "A_Ae_ice": 1., "tp": 195.20}
@@ -174,13 +174,15 @@ for planet in planet_data.keys():
 	
 	planet_data[planet]["tpPressure"] = {}
 	
+	'''DEPRECATED
 	for species in spec_data.keys():
 		print("\tFinding fit for reference triple point pressure for %s"%(species))
 		tp = spec_data[species]["tp"]
 		tpP = 10.**(newtons(fT, tp, np.log10(5000.)))
 		print("\tTriple point found at %.2f mbar"%(tpP))
-		planet_data[planet]["tpPressure"][species] = tpP
-	'''
+		planet_data[planet]["tpPressure"][species] = tpP'''
+
+	''' UNNECESSARY
 	#Plot the data
 	plt.figure()
 	plt.plot(data[:,1],data[:,0],'ro')
@@ -196,25 +198,37 @@ for planet in planet_data.keys():
 ## Setup pressure intervals for vT calculation
 P = {}
 Pref = 1000.
-#P["H2O"] = np.asarray([5000., 4750., 4500., 4000., 3500.,3000., 2500., 2000., 1000.])
-#P["NH3"] = np.asarray([1000., 850., 700., 600., 500., 400.])
+
+''' Old pressure values #P["H2O"] = np.asarray([5000., 4750., 4500., 4000., 3500.,3000., 2500., 2000., 1000.])
+#P["NH3"] = np.asarray([1000., 850., 700., 600., 500., 400.])'''
 
 P["H2O"] = np.linspace(1000.,5000.,10)
 P["NH3"] = np.linspace(400.,1000.,10)
+
+## x, y, gamma, vT and chi are dictionaries which are organized as follows:
+## [Variable] - either x, y, gamma or chi^2 of the fit
+## 		First level - Species
+##			Second level - Planet
+##				Third level - Phase
+## 					Fourth level - value of variable of Phase of Species at Planet.
+## So to get the value of x for NH3 ice on Jupiter, you use:
+## 		x["NH3"]["Jupiter"]["ice"]
 
 ## Set up terminal velocity dictionary
 vT = {}
 
 ## Fit parameters
+
 x = {}
 y = {}
 gamma = {}
 chi = {}
 
+'''DEPRECATED
 x2 = {}
 y2 = {}
 gamma2 = {}
-chi2 = {}
+chi2 = {}'''
 
 ## Run terminal velocity for each species for each planet
 for species in spec_data.keys():
@@ -226,10 +240,11 @@ for species in spec_data.keys():
 	gamma[species] = {}
 	chi[species] = {}
 	
+	'''DEPRECATED
 	x2[species] = {}
 	y2[species] = {}
 	gamma2[species] = {}
-	chi2[species] = {}
+	chi2[species] = {}'''
 	
 	for planet in planet_data.keys():
 		print("\tPlanet: %s"%(planet))
@@ -239,10 +254,11 @@ for species in spec_data.keys():
 		gamma[species][planet] = {}
 		chi[species][planet] = {}
 		
+		'''DEPRECATED
 		x2[species][planet] = {}
 		y2[species][planet] = {}
 		gamma2[species][planet] = {}
-		chi2[species][planet] = {}
+		chi2[species][planet] = {}'''
 		
 		for phase in ["rain","ice","snow"]:
 			print("\t\tPhase: %s"%(phase))
@@ -261,9 +277,13 @@ for species in spec_data.keys():
 			
 			print("\t\t\tCalculating terminal velocity profile")
 			
+			## Initiaze the vT array with empty array
 			for p in P[species]:
 				vT[species][planet][phase][p] = np.zeros(len(D))
 			
+			
+			## This is formatting the vT data to save as .csv
+			## In case we need to see the raw data
 			vT_dat = np.zeros((len(D),2*len(P[species])+2))
 			header = ""
 			for i,p in enumerate(P[species]):
@@ -274,15 +294,19 @@ for species in spec_data.keys():
 				vT_dat[:,2*i+1] = vT_d
 			
 			## save vT data to csv
-			
 			outname = "%s_%s_%s.csv"%(planet,species,phase)
 			np.savetxt(outname,vT_dat,header=header,delimiter=",")
 			
+			''' DEPRECATED
+			## Calculate the terminal velocity and save it to the dictionary at the triple-point pressure
 			vT_d = vT_calc(planet,species, phase, planet_data[planet]["tpPressure"][species], D, planet_data, spec_data)
-			vT[species][planet][phase][planet_data[planet]["tpPressure"][species]] = vT_d
+			vT[species][planet][phase][planet_data[planet]["tpPressure"][species]] = vT_d'''
 
 			gamma_vals = []			
-
+						
+			## Loop through each pressure to calculate the gamma fit for (P0/P)
+			## log(vT(P)/vT(P0)) = gamma*np.log(P0/P)
+			
 			for p in P[species]:
 				if(p != Pref):
 					gamma_p = np.average(np.log(vT[species][planet][phase][p]/vT[species][planet][phase][Pref])/np.log(Pref/p))
@@ -291,8 +315,9 @@ for species in spec_data.keys():
 			gamma[species][planet][phase] = np.average(gamma_vals)
 			
 			gamma_vals = []
-			Pref2 = planet_data[planet]["tpPressure"][species]
 			
+			'''DEPRECATED
+			Pref2 = planet_data[planet]["tpPressure"][species]
 
 			for p in P[species]:
 				if(p != Pref2):
@@ -300,11 +325,11 @@ for species in spec_data.keys():
 					gamma_vals.append(gamma_p)
 			
 			gamma2[species][planet][phase] = np.average(gamma_vals)
-			## Convert 2D array of pressure and particle size to 1D
-			## Also convert vT to 1D array
+			'''
+			
+			## Convert 2D array of pressure and particle size to 1D (xData)
+			## Also convert vT to 1D array (zData)
 			## For fitting
-			
-			
 			xData = []
 			zData = []
 			
@@ -324,48 +349,65 @@ for species in spec_data.keys():
 			x[species][planet][phase] = 10.**par[0]
 			y[species][planet][phase] = par[1]
 			
+			
+			'''DEPRECATED
 			## Run with new triple-point reference pressure
 			xData2 = []
 			zData2 = []
 			
-			Pref2 = planet_data[planet]["tpPressure"][species]
-			print("\t\tCreating array to fit")
-			for i in range(len(P[species])):
-				for j in range(len(D)):
-					#index = (i*len(P[species])) + j
-					xData2.append(D[j])
-					zData2.append(vT[species][planet][phase][P[species][i]][j]/((Pref2/P[species][i])**gamma2[species][planet][phase]))
-			xData2 = np.asarray(xData2)
-			zData2 = np.asarray(zData2)
-			
-			print("\t\tFitting with triple point reference")
-			par2, cov2 = curve_fit(vT_fit, np.log10(xData2),np.log10(zData2))
-			
-			
-			x2[species][planet][phase] = 10.**par2[0]*((Pref2/Pref)**(gamma2[species][planet][phase]))
-			y2[species][planet][phase] = par2[1]			
+
+				Pref2 = planet_data[planet]["tpPressure"][species]
+				print("\t\tCreating array to fit")
+				for i in range(len(P[species])):
+					for j in range(len(D)):
+						#index = (i*len(P[species])) + j
+						xData2.append(D[j])
+						zData2.append(vT[species][planet][phase][P[species][i]][j]/((Pref2/P[species][i])**gamma2[species][planet][phase]))
+				xData2 = np.asarray(xData2)
+				zData2 = np.asarray(zData2)
+				
+				print("\t\tFitting with triple point reference")
+				par2, cov2 = curve_fit(vT_fit, np.log10(xData2),np.log10(zData2))
+				
+				
+				x2[species][planet][phase] = 10.**par2[0]*((Pref2/Pref)**(gamma2[species][planet][phase]))
+				y2[species][planet][phase] = par2[1]		'''
 
 xtheo = {"NH3": {"snow": 48.606, "rain": 2479.}, "H2O": {"snow": 50.172,"rain": 2615.1}}
 ytheo = {"NH3": {"snow": 0.48803, "rain": 0.76217}, "H2O": {"snow": 0.47798,"rain": 0.74245}}
 gammatheo = {"snow": 0.47, "rain": 0.33}
+
 ## Plot parameters	
-pltspec = "NH3"
+pltspec = "H2O"
 pltplanet = "Saturn"
 pltphase = "snow"
 pltP = 1000.
 vTp = vT[pltspec][pltplanet][pltphase]
 
+## Initialize a figure
 fig1 = plt.figure()
 ax1 = fig1.add_subplot(111)
+
+## Plot the vT points for the given plot paramter
 ax1.plot(vTp["D"]*1000.,vTp[pltP],'k.')
+
+## Get the fitted curve for the plot paramter
 fitted = x[pltspec][pltplanet][pltphase]*(vTp["D"]**y[pltspec][pltplanet][pltphase])*(Pref/pltP)**gamma[pltspec][pltplanet][pltphase]
-#chi = np.sum(((vT[pltspec][pltplanet][pltphase][pltP] - fitted)**2.)/vT[pltspec][pltplanet][pltphase][pltP])
+
+## Calculate R^2 statistic
 sstot = np.sum((vTp[pltP] - np.average(vTp[pltP]))**2.)
 ssres = np.sum((vTp[pltP] - fitted)**2.)
 R2 = 1. - ssres/sstot
-ax1.plot(vTp["D"]*1000.,fitted,'k-')
-ax1.text(0.1,0.5,"R2: %.4f"%(R2))
 
+## Plot the fitted curve as a - line
+ax1.plot(vTp["D"]*1000.,fitted,'k-')
+
+## Annotate with R2 text
+## The text function displays the text at the relative axis coordinate (first two numbers)
+## So 0, 0 is the bot-left and 1,1 is the top-right and 0.5, 0.5 is in the middle of the plot
+ax1.text(0.1,0.5,"R2: %.4f"%(R2),fontsize=12,transform=ax1.transAxes)
+
+'''DEPRECATED
 fig2 = plt.figure()
 ax2 = fig2.add_subplot(111)
 if(False):
@@ -401,6 +443,20 @@ for species in spec_data.keys():
 		print("x: %.4f \t x2 : %.4f"%(x[species][printplanet][phase],x2[species][printplanet][phase]))
 		print("y: %.4f \t y2 : %.4f"%(y[species][printplanet][phase],y2[species][printplanet][phase]))
 		print("gamma: %.4f \t gamma2 : %.4f"%(gamma[species][printplanet][phase],gamma2[species][printplanet][phase]))
+		print()
+	print()
+plt.show()
+'''
+
+printplanet = "Jupiter"
+for species in spec_data.keys():
+	print(species)
+	print("----------------")
+	for phase in ["ice","snow","rain"]:
+		print("Phase: %s"%(phase))
+		print("x: %.4f"%(x[species][printplanet][phase]))
+		print("y: %.4f"%(y[species][printplanet][phase]))
+		print("gamma: %.4f"%(gamma[species][printplanet][phase]))
 		print()
 	print()
 plt.show()
